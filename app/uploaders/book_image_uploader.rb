e13905c06ad0a482a6595bbf9d480c70fb9fd7c6
chisato_sugawara <chi.sugawara1116@gmail.com>
2023-01-30 17:50:25 +0900

    fix: 画像アップロード先の修正

--- app/uploaders/book_image_uploader.rb
+++ app/uploaders/book_image_uploader.rb
@@ -14,9 +14,6 @@ class BookImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-  def store_dir
     if Rails.env.production?
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     else
