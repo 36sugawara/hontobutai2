11a18a169e5de3f4fcc0e228396e4d96f79e3bb9
chisato_sugawara <chi.sugawara1116@gmail.com>
2023-02-07 08:39:36 +0900

    fix: 余分なコメントアウトの削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,12 +8,12 @@ module ApplicationHelper
     google_book['volumeInfo'].slice('title', 'authors', 'publishedDate', 'infoLink', 'bookImage')
   end
 
-  def page_title(page_title = '')#, admin: false)
-    base_title = #if admin
-                  #'Hontobutai | 管理画面'
-                #else
+  def page_title(page_title = '', admin: false)
+    base_title = if admin
+                  'Hontobutai | 管理画面'
+                else
                   'Hontobutai'
-                #end
+                end
 
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
